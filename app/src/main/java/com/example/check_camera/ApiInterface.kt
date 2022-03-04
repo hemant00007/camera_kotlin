02b8b04520c8c84867a160d95676631d8a36629d
hemant Kumar <hemantkumar692@gmail.com>
2022-03-04 12:12:30 +0530

    image upload in multipart form data

---
+++ app/src/main/java/com/example/check_camera/ApiInterface.kt
@@ -0,0 +1,39 @@
+package com.example.check_camera
+
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface ApiInterface {
+    @Multipart
+    @POST("post")
+    fun getpost(
+        @Part("name") name: RequestBody,
+        @Part("mobile") mobile : RequestBody,
+        @Part("email") emailid : RequestBody,
+        @Part image : MultipartBody.Part,
+        @Part("created_date") datee : RequestBody
+    ): Call<Mymodel>
+
+    companion object {
+
+        var BASE_URL = "http://172.20.3.87/daily_visit/test/"
+
+        fun create() : ApiInterface {
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(ApiInterface::class.java)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/check_camera/Mymodel.kt
@@ -0,0 +1,7 @@
+package com.example.check_camera
+
+data class Mymodel(
+    val errors: Boolean,
+    val message: String,
+    val status: Boolean
+)
